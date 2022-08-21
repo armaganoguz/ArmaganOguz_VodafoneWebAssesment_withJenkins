1a2e4da12909a89a977048e023e4529c3e8ff7a9
armaganoguz <chevkimusic@gmail.com>
2022-08-22 00:29:58 +0300

    first commit

--- eclipse-workspace/ArmaganOguz_WebAssesment/src/test/java/StepDefinitions/SignupSteps.java
+++ eclipse-workspace/ArmaganOguz_WebAssesment/src/test/java/StepDefinitions/SignupSteps.java
@@ -18,7 +18,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-
+ 
 public class SignupSteps {
 
 	ChromeOptions options;
